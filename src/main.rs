52b38fc0179e224741119eedca87650f7720dba1
YuzuRyo61 <yuzuryo61@yuzulia.work>
2021-01-16 14:48:50 +0900

    Functionize Clap settings

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
 //! # YuzuRyo61
 //! Introduction program of YuzuRyo61 profile.
 
-#[macro_use]
-extern crate clap;
-
-use clap::{SubCommand, AppSettings, Arg};
-use yuzuryo61::variables::{BLOG_URL, SITE_URL};
+use yuzuryo61::variables::{BLOG_URL, SITE_URL, get_clap_app};
 
 /// メインプログラム
 fn main() -> std::io::Result<()> {
-    let app = app_from_crate!()
-        .name("YuzuRyo61")
-        .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(SubCommand::with_name("site")
-            .about("Open browser(or print URL) to my site")
-            .arg(Arg::with_name("no_open")
-                .short("o")
-                .long("no-open")
-                .help("Do not open the browser")
-                .required(false)
-                .takes_value(false))
-        )
-        .subcommand(SubCommand::with_name("blog")
-            .about("Open browser(or print URL) to blog site")
-            .arg(Arg::with_name("no_open")
-                .short("o")
-                .long("no-open")
-                .help("Do not open the browser")
-                .required(false)
-                .takes_value(false))
-        );
+    let app = get_clap_app();
     let matches = app.get_matches();
 
     if let Some(am) = matches.subcommand_matches("site") {
--- src/variables.rs
+++ src/variables.rs
@@ -1,2 +1,28 @@
+use clap::{SubCommand, AppSettings, Arg, App};
+
 pub const SITE_URL: &str = "https://yuzuryo61.me/";
 pub const BLOG_URL: &str = "https://yuzulia.com/";
+
+pub fn get_clap_app<'a, 'b>() -> App<'a, 'b> {
+    app_from_crate!()
+        .name("YuzuRyo61")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(SubCommand::with_name("site")
+            .about("Open browser(or print URL) to my site")
+            .arg(Arg::with_name("no_open")
+                .short("o")
+                .long("no-open")
+                .help("Do not open the browser")
+                .required(false)
+                .takes_value(false))
+        )
+        .subcommand(SubCommand::with_name("blog")
+            .about("Open browser(or print URL) to blog site")
+            .arg(Arg::with_name("no_open")
+                .short("o")
+                .long("no-open")
+                .help("Do not open the browser")
+                .required(false)
+                .takes_value(false))
+        )
+}
